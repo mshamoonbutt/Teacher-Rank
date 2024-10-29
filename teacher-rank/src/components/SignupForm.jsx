4de3222f78844f20204423d91d9dec8003fe93ab
Shamoon <271046574@formanite.fccollege.edu.pk>
2024-10-29 17:30:17 +0500

    frontend signup form

--- teacher-rank/src/components/SignupForm.jsx
+++ teacher-rank/src/components/SignupForm.jsx
@@ -14,14 +14,27 @@ const SignupForm = () => {
       <h1 className="signup-title">SIGN UP</h1>
       
       <div className="signup-box">
+
+        <label>Name</label>
+        <input type="text" placeholder="Value" />
+
         <label>Email(FCCU)</label>
         <input type="email" placeholder="Value" />
 
         <label>Semester</label>
-        <input type="text" placeholder="Value" />
+        <select name="semester" required>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+
 
         <label>Major</label>
-        <input type="text" placeholder="Value" />
+        <select name="major" required>
+          <option value="computer-science">Computer Science</option>
+          <option value="business">Business</option>
+          <option value="technology">Technology</option>
+        </select>
 
         <label>Screenshot of Transcript</label>
         <textarea placeholder="Value"></textarea>
